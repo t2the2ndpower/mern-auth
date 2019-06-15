7c21bb89e012f734db658337768c691833ba4e68
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-06-15 12:57:21 -0400

    auth tute updates

--- server.js
+++ server.js
@@ -38,4 +38,4 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.listen(port, () => console.log(`Server is up and running on port ${port} !`));
