306f2bbac9be8adaddccbc44924d3ba02403aaea
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-06-15 10:08:59 -0400

    mern auth worki

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,19 @@
 module.exports = {
-  mongoURI: "YOUR_MONGO_URI_HERE",
+  mongoURI: "mongodb+srv://technoladie_db:M6JS0QjaS57x5w8o@ids3g0-usoyv.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "secret"
+  // mongo "mongodb+srv://ids3g0-usoyv.mongodb.net/test" --username technoladie_db
+  // or
+  // mongodb+srv://technoladie_db:M6JS0QjaS57x5w8o@ids3g0-usoyv.mongodb.net/test?retryWrites=true&w=majority
+  //  the api key 48cd31d1-b992-43af-bda4-5a603488d291
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://technoladie_db:<password>@ids3g0-usoyv.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 };
